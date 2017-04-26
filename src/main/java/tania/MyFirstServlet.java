51df4a5ba2a46c8adbbb7fc3c4f45fc538dde87a
tania <tania.nik596@gmail.com>
2017-04-26 13:22:07 +0300

    simple form for writing messages added

--- src/main/java/tania/MyFirstServlet.java
+++ src/main/java/tania/MyFirstServlet.java
@@ -1,14 +1,22 @@
+//сдеелать форму, куда вписывается текст и потом выводится на новой стр
+
 package tania;
 
+import javax.servlet.*;
 import javax.servlet.http.*;
+import java.io.*;
 
 public class MyFirstServlet extends HttpServlet {
-    public void doGet(HttpServletRequest request, HttpServletResponse response) {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setStatus(200);
-        try {
-            response.getWriter().write("Hello, Servlet!");
-        } catch (Exception exception){
-            exception.printStackTrace();
-        }
+        String writtentext = (String) request.getParameter("writtentext");
+        response.setContentType("text/html;charset=UTF-8");
+        response.getWriter().println("<!DOCTYPE HTML>");
+        response.getWriter().println("<html><body><p>" + writtentext + "</p></body></html>");
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
     }
 }
