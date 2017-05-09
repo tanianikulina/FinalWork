bde47652a07fa809710831e34671283f820881e5
tania <tania.nik596@gmail.com>
2017-05-09 21:44:55 +0300

    form for history added

--- src/main/java/tania/MyFirstServlet.java
+++ src/main/java/tania/MyFirstServlet.java
@@ -4,7 +4,6 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.*;
 import java.time.Instant;
-import java.util.ArrayList;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -13,23 +12,44 @@ public class MyFirstServlet extends HttpServlet {
     ConcurrentHashMap <Instant, String> messages = new ConcurrentHashMap<>();
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setStatus(200);
-        response.setHeader("Content-type", "application/json:charset=UTF-8");
+//        response.setStatus(200);
+//        response.setHeader("Content-type", "application/json:charset=UTF-8");
         StringBuilder responseText = new StringBuilder();
         responseText.append("[");
         try {
             List<String> messagesList = new ArrayList<String>(messages.values());
             messagesList.stream().limit(messagesList.size() -1).forEach((message) ->
-            responseText.append("\"" + message + "\",")
+                responseText.append("\"" + message + "\",")
         );
-            if (messagesList.size() > 0)
-                responseText.append("\"" + messagesList.get(messagesList.size() - 1) + "\"]");
+            if (messagesList.size() > 0) {
+                messagesList.stream().limit(messagesList.size()-1).forEach((message) -> {
+                    responseText.append("\"" + message + "\",");
+                });
+                responseText.append("\"" + messagesList.get(messagesList.size() - 1) + "\"");
+            }
+            responseText.append("]");
+            System.out.println(responseText.toString());
+            response.getWriter().write(responseText.toString());
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        String message = "";
+        try {
+            message = request.getReader().readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (message.length() > 0) {
+            try {
+                System.out.println(message);
+                messages.put(Instant.now(), message);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        response.setStatus(200);
     }
 }
---
+++ src/main/webapp/script.js
@@ -0,0 +1,36 @@
+function sendMessage(message) {
+    jQuery.post("/messages", message, function () {
+    getNewMessages();
+    }, "application/json")
+}
+
+function updateMessagesList(newMessages) {
+    var currentMessagesList = jQuery(".chat_messages_list");
+    //var currentMessages = currentMessagesList.val();
+    var currentMessages = "";
+    var newMessageList = JSON.parse(newMessages);
+    newMessages.forEach(function (message) {
+        currentMessages += message + "\n";
+    });
+    currentMessagesList.val(currentMessages)
+}
+
+function getNewMessages() {
+    var now = (new Date()).toISOString();
+    jQuery.get("/messages?date_time=" + now, null, function (newMessages) {
+        updateMessagesList(newMessages);
+    });
+}
+
+function getOldMessages() {
+    jQuery.get("/messages?first_time", null, function (newMessages) {
+        updateMessagesList(newMessages);
+    });
+}
+
+jQuery(document).ready(function () {
+    jQuery(".chat_submit_button").click(function () {
+        sendMessage(jQuery(".chat_new_message").val());
+    });
+    setInterval(getNewMessages, 5000)
+});
