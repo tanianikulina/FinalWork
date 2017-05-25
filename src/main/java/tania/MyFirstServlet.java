c889a92969180716b3f5a0d455a4dbd001d82cba
tania <tania.nik596@gmail.com>
2017-05-26 01:53:43 +0300

    пересдача (2)

--- src/main/java/tania/MyFirstServlet.java
+++ src/main/java/tania/MyFirstServlet.java
@@ -12,12 +12,12 @@ public class MyFirstServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
         StringBuilder responseText = new StringBuilder();
-        ResultSet resultSet;
-        List<String> messagesList = new ArrayList<>();
 
         response.setHeader("Content-type", "application/json:charset=UTF-8");
         responseText.append("[");
         try {
+            ResultSet resultSet;
+            List<String> messagesList = new ArrayList<>();
             DBStuff db = new DBStuff();
             if (request.getParameter("time") != null) {
                 resultSet = db.getMessages(request.getParameter("time"));
@@ -27,12 +27,12 @@ public class MyFirstServlet extends HttpServlet {
             while (resultSet.next())
                 messagesList.add(resultSet.getString("time") + " " + resultSet.getString("login") + ": " + resultSet.getString("message"));
             db.close();
+            if (messagesList.size() > 0) {
+                messagesList.stream().limit(messagesList.size() - 1).forEach((message) -> responseText.append("\"").append(message).append("\","));
+                responseText.append("\"").append(messagesList.get(messagesList.size() - 1)).append("\"");
+            }
         } catch (Exception e) {
             e.printStackTrace();
-        }
-        if (messagesList.size() > 0) {
-            messagesList.stream().limit(messagesList.size() - 1).forEach((message) -> responseText.append("\"").append(message).append("\","));
-            responseText.append("\"").append(messagesList.get(messagesList.size() - 1)).append("\"");
         }
         responseText.append("]");
         response.getWriter().write(responseText.toString());
@@ -40,16 +40,17 @@ public class MyFirstServlet extends HttpServlet {
     }
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        UUID token = UUID.fromString(request.getParameter("token"));
-        String message = request.getParameter("message");
         try {
+            UUID token = UUID.fromString(request.getParameter("token"));
+            String message = request.getParameter("message");
             DBStuff db = new DBStuff();
             if (message.length() > 0)
                 db.putMessage(db.getAuthor(token), message, Instant.now());
             db.close();
+            response.setStatus(200);
         } catch (Exception e) {
             e.printStackTrace();
+            response.setStatus(500);
         }
-        response.setStatus(200);
     }
 }
